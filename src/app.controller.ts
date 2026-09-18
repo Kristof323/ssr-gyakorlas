3791fc12fd5089ecf17675cf05cfaa66d7aa8895
Kristof323 <kristofcsapo@gmail.com>
2026-09-18 10:16:08 +0200

    órai munka

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import fs from 'node:fs';
+import { Criminal } from './Criminal.js';
 
 @Controller()
 export class AppController {
@@ -12,4 +14,36 @@ export class AppController {
       title: 'My First NestJS App'
     }
   }
+
+ @Get('Piros-Kek')
+ @Render('red-blue')
+ GetRedBlue() {
+      const random = Math.random();
+      const bgColor = random < 0.5 ? 'red' : 'blue';
+    return {
+      bgColor
+    }
+ }
+
+ @Get('wanted')
+ @Render('wanted')
+getWanted() {
+  const criminal = JSON.parse(
+    fs.readFileSync('wanted.json',{encoding: 'utf-8'})
+  )
+    return { criminal }
+  }
+
+  @Get('searchCrime')
+  searchCrime(@Query('keresett') keresett: string)
+  {
+    const criminal = JSON.parse(
+      fs.readFileSync('wanted.json',{encoding: 'utf-8'})
+    ) as Criminal;
+    
+    return {
+      talalatok: criminal.crimes
+      .filter(crime => crime.toLowerCase().includes(keresett.toLowerCase()))
+    }
+  } 
 }
